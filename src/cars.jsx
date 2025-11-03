84740156afdddf465789dab8c823b68514eedaf1
MarcoKeyzer <Marcojkeyzer@gmail.com>
2025-11-03 23:27:35 +0200

    filter list by current user

--- src/cars.jsx
+++ src/cars.jsx
@@ -1,23 +1,29 @@
-import { DataTable, List ,Edit, SimpleForm, TextInput, ReferenceField, BooleanField, Create, TextField } from 'react-admin';
+import { DataTable, List ,Edit, SimpleForm, TextInput, ReferenceField, Create, TextField, useGetIdentity, HIDE_FILTER, Show } from 'react-admin';
 
-export const CarsList = () => (
-    <List>
+export const CarsList = () => {
+    const { identity } = useGetIdentity();
+    
+    const filter = {user_id: identity?.id}  //create a filter to compare item user_id and current user's id
+
+    return (
+    <List filter={filter}> {/* set filter on list */}
+    
         <DataTable>
             <DataTable.Col source="model" />
             <DataTable.Col source="brand" />
             <DataTable.Col source="year" />
-            <DataTable.Col label="FK email">{/*name our new column*/}
+            <DataTable.Col label="Owner">
                 <ReferenceField 
-                    source="user_id"        //link user_id from current table
-                    reference="users"       //to users table
+                    source="user_id"        
+                    reference="users"       
                 >
-                    {/* get email column from users table */}
                     <TextField source="email"></TextField>   
                 </ReferenceField>
             </DataTable.Col>
         </DataTable>
     </List>
-);
+    )
+}
 
 export const CarsEdit = () => (
     <Edit>
@@ -29,12 +35,18 @@ export const CarsEdit = () => (
     </Edit>
 );
 
-export const CarsCreate = () => (
-    <Create>
-        <SimpleForm>
-            <TextInput source="model" />
-            <TextInput source="brand" />
-            <TextInput source="year" />
-        </SimpleForm>
-    </Create>
-);
+export const CarsCreate = () => {
+    const { identity } = useGetIdentity();
+
+    const defaultValues = { user_id: identity?.id };
+
+    if (identity) return (
+        <Create>
+            <SimpleForm defaultValues={defaultValues}>
+                <TextInput source="model" />
+                <TextInput source="brand" />
+                <TextInput source="year" />
+            </SimpleForm>
+        </Create>
+    );
+}
